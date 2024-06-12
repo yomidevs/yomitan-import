d1bc26d83b5482664305cf3849ee5f1e9be7dfca
kuuuube <hexagonisalie@gmail.com>
2024-06-11 20:25:02 -0400

    Fix cc target

--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -4,7 +4,7 @@ build_dir="yomitan-import-windows"
 
 mkdir -p "$build_dir"
 
-CGO_ENABLED=1 CC=/usr/bin/x86_64-w64-mingw32-g++ CXX=/usr/bin/x86_64-w64-mingw32-g++ GOOS=windows OARCH=amd64 go build -o "yomitan-import-windows" -ldflags '-extldflags "-static"' ./yomitan
-CGO_ENABLED=1 CC=/usr/bin/x86_64-w64-mingw32-g++ CXX=/usr/bin/x86_64-w64-mingw32-g++ GOOS=windows OARCH=amd64 go build -o "yomitan-import-windows" -ldflags '-extldflags "-static"' ./yomitan-gtk
+CGO_ENABLED=1 CC=/usr/bin/x86_64-w64-mingw32-gcc CXX=/usr/bin/x86_64-w64-mingw32-g++ GOOS=windows OARCH=amd64 go build -o "yomitan-import-windows" -ldflags '-extldflags "-static"' ./yomitan
+CGO_ENABLED=1 CC=/usr/bin/x86_64-w64-mingw32-gcc CXX=/usr/bin/x86_64-w64-mingw32-g++ GOOS=windows OARCH=amd64 go build -o "yomitan-import-windows" -ldflags '-extldflags "-static"' ./yomitan-gtk
 
 zip -r "$build_dir.zip" "$build_dir"
