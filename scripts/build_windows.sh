78a1e767dcf1f14d2a8b5e1e34c175a5832d3d60
kuuuube <hexagonisalie@gmail.com>
2024-06-11 20:07:25 -0400

    Fix build architectures

--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -4,7 +4,7 @@ build_dir="yomitan-import-windows"
 
 mkdir -p "$build_dir"
 
-CGO_ENABLED=1 CC=/usr/bin/i686-w64-mingw32-g++ GOOS=windows OARCH=x64 go build -o "yomitan-import-windows" ./yomitan
-CGO_ENABLED=1 CC=/usr/bin/i686-w64-mingw32-g++ GOOS=windows OARCH=x64 go build -o "yomitan-import-windows" ./yomitan-gtk
+CGO_ENABLED=1 CC=/usr/bin/x86_64-w64-mingw32-g++ GOOS=windows OARCH=amd64 go build -o "yomitan-import-windows" ./yomitan
+CGO_ENABLED=1 CC=/usr/bin/x86_64-w64-mingw32-g++ GOOS=windows OARCH=amd64 go build -o "yomitan-import-windows" ./yomitan-gtk
 
 zip -r "$build_dir.zip" "$build_dir"
